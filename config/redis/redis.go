31c157c85c900ac080b61d5dbf8bd665c5f20d2a
agrha prayogo <agrha_a152@mitrais.com>
2024-01-30 15:41:11 +0700

    feat docker redis

--- config/redis/redis.go
+++ config/redis/redis.go
@@ -9,12 +9,12 @@ import (
 var redisClient *redis.Client
 
 func Connection() *redis.Client {
-	redisConnect := os.Getenv("REDIS_SERVER")
+	redisServer := os.Getenv("REDIS_SERVER")
 	// Initialize Redis connection
 	redisClient = redis.NewClient(&redis.Options{
-		Addr:     redisConnect, // Your Redis server address
-		Password: "",           // No password
-		DB:       0,            // Default DB
+		Addr:     redisServer, // Your Redis server address
+		Password: "",          // No password
+		DB:       0,           // Default DB
 	})
 	return redisClient
 }
