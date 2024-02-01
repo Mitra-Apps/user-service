9d6c489189aae71442f9283b705b2df80dfa7a82
nanda <nurcholis.nanda@gmail.com>
2024-02-01 14:04:56 +0700

    update response from using anypb to structpb

--- handler/grpc/grpc.go
+++ handler/grpc/grpc.go
@@ -8,8 +8,8 @@ import (
 	"github.com/Mitra-Apps/be-user-service/service"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"google.golang.org/protobuf/types/known/anypb"
 	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/structpb"
 )
 
 type GrpcRoute struct {
@@ -51,14 +51,16 @@ func (g *GrpcRoute) Login(ctx context.Context, req *pb.UserLoginRequest) (*pb.Su
 	if err != nil {
 		return nil, err
 	}
-	token := &pb.UserLoginResponse{
-		AccessToken:  jwt.AccessToken,
-		RefreshToken: jwt.RefreshToken,
+	token := map[string]interface{}{
+		"access_token":  jwt.AccessToken,
+		"refresh_token": jwt.RefreshToken,
 	}
-	data, err := anypb.New(token)
+
+	data, err := structpb.NewStruct(token)
 	if err != nil {
 		return nil, status.Errorf(codes.InvalidArgument, err.Error())
 	}
+
 	return &pb.SuccessResponse{
 		Data: data,
 	}, nil
@@ -72,13 +74,14 @@ func (g *GrpcRoute) Register(ctx context.Context, req *pb.UserRegisterRequest) (
 	if err != nil {
 		return nil, err
 	}
-	otpProto := &pb.UserRegisterResponse{
-		Otp: otp,
+	otpStruct := map[string]interface{}{
+		"otp": otp,
 	}
-	data, err := anypb.New(otpProto)
+	data, err := structpb.NewStruct(otpStruct)
 	if err != nil {
 		return nil, status.Errorf(codes.InvalidArgument, err.Error())
 	}
+
 	return &pb.SuccessResponse{
 		Data: data,
 	}, nil
@@ -108,7 +111,10 @@ func (g *GrpcRoute) GetRole(ctx context.Context, req *emptypb.Empty) (*pb.Succes
 	roleData := &pb.ListRole{
 		Roles: protoRoles,
 	}
-	data, err := anypb.New(roleData)
+	roleStruct := map[string]interface{}{
+		"roles": roleData,
+	}
+	data, err := structpb.NewStruct(roleStruct)
 	if err != nil {
 		return nil, status.Errorf(codes.InvalidArgument, err.Error())
 	}
